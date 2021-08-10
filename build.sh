b9ee44bebd12b9edb8da6b5b34b801598db833b3
Tata Lee <67614323+macukex1991@users.noreply.github.com>
2021-08-10 10:46:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,37 +2,33 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni -b twrp-10.0"
+MANIFEST="https://gitlab.com/OrangeFox/sync.git"
 DEVICE=LD7
-DT_LINK="https://github.com/macukex1991/Tecno-LD7-TWRP"
+DT_LINK="https://github.com/macukex1991/android_device_TECNO_LD7 -b android-10"
 DT_PATH=device/TECNO/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
 apt update --fix-missing
 apt install openssh-server -y
-mkdir ~/twrp10 && cd ~/twrp10
+#mkdir ~/twrp10 && cd ~/twrp10
 
 echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST
-repo sync
-git clone --depth=1 $DT_LINK $DT_PATH
+git clone $MANIFEST k
+cd k
+./get_fox_10.sh ~/work
+cd ~/work
+git clone $DT_LINK --depth=1 --single-branch $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
-. build/envsetup.sh
-export TW_THEME=portrait_hdpi
-export ALLOW_MISSING_DEPENDENCIES=true
-lunch omni_${DEVICE}-eng && mka recoveryimage
+cd ~/work
+ . build/envsetup.sh
+ export ALLOW_MISSING_DEPENDENCIES=true
+ lunch omni_$DEVICE-eng && mka recoveryimage
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
-version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 
-cd out/target/product/$DEVICE
-mv recovery.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
-
-#curl -T $OUTFILE https://oshi.at
-curl -sL $OUTFILE https://git.io/file-transfer | sh
+cd ~/work/out/target/product/*/*.zip
+curl -sL https://git.io/file-transfer | sh
 ./transfer wet *.zip
