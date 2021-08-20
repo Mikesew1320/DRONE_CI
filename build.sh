1f2e17f261b7ce39addc0d00401d3d49b03e313d
Mikesew1320 <43565227+Mikesew1320@users.noreply.github.com>
2021-08-20 23:50:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,33 +2,37 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://gitlab.com/OrangeFox/sync.git"
+MANIFEST="git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0"
 DEVICE=CD6
-DT_LINK="https://github.com/Mikesew1320/twrp_device_TECNO_CD6"
+DT_LINK="https://github.com/Mikesew1320/TECNO-CD6-SHRP"
 DT_PATH=device/TECNO/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
 apt update --fix-missing
 apt install openssh-server -y
-#mkdir ~/twrp10 && cd ~/twrp10
-#fix
+mkdir ~/twrp11 && cd ~/twrp11
+
 echo " ===+++ Syncing Recovery Sources +++==="
-git clone $MANIFEST k
-cd k
-./get_fox_10.sh ~/work
-cd ~/work
-git clone $DT_LINK --depth=1 --single-branch $DT_PATH
+repo init --depth=1 -u $MANIFEST
+repo sync
+git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
-cd ~/work
- . build/envsetup.sh
- export ALLOW_MISSING_DEPENDENCIES=true
- lunch omni_$DEVICE-eng && mka recoveryimage
+. build/envsetup.sh
+#export TW_THEME=portrait_hdpi
+export ALLOW_MISSING_DEPENDENCIES=true
+lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
+echo " lunch twrp_${DEVICE}-eng done"
+mka recoveryimage || abort " mka failed with exit status $?"
+echo " mka recoveryimage done"
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
+version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
+OUTFILE=SHRP-3.1-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 
-cd ~/work/out/target/product/CD6
+cd out/target/product/$DEVICE
 curl -sL https://git.io/file-transfer | sh
+./transfer $OUTFILE  *.zip
 ./transfer wet *.zip
