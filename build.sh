f57cc5bf520cd27fa1724505682af6393e025769
Mikesew1320 <43565227+Mikesew1320@users.noreply.github.com>
2021-08-21 12:47:03 +0300

    Don't know what can i do!

--- build.sh
+++ build.sh
@@ -4,31 +4,35 @@
 
 MANIFEST="git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0"
 DEVICE=CD6
-DT_LINK=https://github.com/Mikesew1320/TECNO-CD6-SHRP-Tree -b android-10.0"
+DT_LINK="https://github.com/Mikesew1320/TECNO-CD6-SHRP-Tree"
 DT_PATH=device/TECNO/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
 apt update --fix-missing
 apt install openssh-server -y
-mkdir ~/shrp && cd ~/shrp
+mkdir ~/twrp11 && cd ~/twrp11
 
 echo " ===+++ Syncing Recovery Sources +++==="
-repo init -u $MANIFEST --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
-repo sync -c -q --force-sync --no-clone-bundle --no-tags -j6
-#repo init --depth=1 -u $MANIFEST
-#repo sync
+repo init --depth=1 -u $MANIFEST
+repo sync
 git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
+#export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch omni_${DEVICE}-eng
-mka recoveryimage
+lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
+echo " lunch twrp_${DEVICE}-eng done"
+mka recoveryimage || abort " mka failed with exit status $?"
+echo " mka recoveryimage done"
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
+version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
+OUTFILE=SHRP-3.1-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 
 cd out/target/product/$DEVICE
 curl -sL https://git.io/file-transfer | sh
+./transfer $OUTFILE  *.zip
 ./transfer wet *.zip
