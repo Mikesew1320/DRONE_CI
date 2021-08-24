dccde119edd1ec56a853357a7be0548783547972
Mikesew1320 <43565227+Mikesew1320@users.noreply.github.com>
2021-08-24 12:28:06 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
-export TW_THEME=portrait_hdpi
+#export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
 echo " lunch twrp_${DEVICE}-eng done"
